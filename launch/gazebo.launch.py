5936a73dfc5202caf7ad8f262f65a6e2855184ea
Devanshu Sharma <devanshu@blackcoffeerobotics.com>
2023-06-09 11:53:31 +0530

    cleanup

--- launch/gazebo.launch.py
+++ launch/gazebo.launch.py
@@ -18,35 +18,42 @@ def get_xacro_to_doc(xacro_file_path, mappings):
     return doc
 
 def generate_launch_description():
+    # Retrieve launch configuration arguments
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
 
-    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+    # Path to the Xacro file
     xacro_path = join(get_package_share_directory('new_bcr_robot'), 'urdf', 'new_bcr_robot.xacro')
     doc = get_xacro_to_doc(xacro_path, {"wheel_odom_topic": "odom", "sim_gazebo": "true"})
 
+    # Launch the robot_state_publisher node
     robot_state_publisher = Node(
-        package='robot_state_publisher', 
-        executable='robot_state_publisher', 
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
         name='robot_state_publisher',
         output='screen',
         parameters=[{'use_sim_time': use_sim_time}, {'robot_description': doc.toxml()}]
     )
 
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+    # Launch the spawn_entity node to spawn the robot in Gazebo
+    spawn_entity = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
         arguments=['-topic', 'robot_description', '-entity', 'new_bcr_robot'],
-        output='screen')
+        output='screen'
+    )
 
+    # Include the Gazebo launch file
     gazebo_share = get_package_share_directory("gazebo_ros")
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(join(gazebo_share, "launch", "gazebo.launch.py"))
     )
 
     return LaunchDescription([
-        DeclareLaunchArgument(
-            'world', default_value=[FindPackageShare('new_bcr_robot'), '/worlds/gazebo/mars.world'],
-        ),
+        # Declare launch arguments
+        DeclareLaunchArgument('world', default_value=[FindPackageShare('new_bcr_robot'), '/worlds/gazebo/mars.world']),
         DeclareLaunchArgument('gui', default_value='true'),
         DeclareLaunchArgument('verbose', default_value='false'),
-        DeclareLaunchArgument('use_sim_time', default_value='false'),
+        DeclareLaunchArgument('use_sim_time', default_value='true'),
         DeclareLaunchArgument('robot_description', default_value=doc.toxml()),
         gazebo,
         robot_state_publisher,
